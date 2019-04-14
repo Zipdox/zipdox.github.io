a2b4e909db0c56a3135c7dcbc7e249a16200c33e
Zipdox <42157341+Zipdox@users.noreply.github.com>
2019-04-14 17:51:21 +0200

    fixed https issues hopefully

--- t/tiktok/scraper.js
+++ t/tiktok/scraper.js
@@ -1,3 +1,7 @@
+if (window.location.href.startsWith("https://")) {
+    window.location.href = window.location.href.replace("https://", "http://");
+}
+
 document.getElementById("download").onclick = function(){
 
     var link = document.getElementById("urlbox").value;
