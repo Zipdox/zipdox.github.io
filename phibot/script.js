b79645ed29882a7bd084a652a6d44378629f2544
Zipdox <42157341+Zipdox@users.noreply.github.com>
2019-06-16 23:39:53 +0200

    Add files via upload

--- phibot/script.js
+++ phibot/script.js
@@ -3,7 +3,7 @@ function escapeHTML(str) {
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
 }
-const commands = [`?discord≫Get the Constantiam Discord link.`,`?mapart≫Get the Constantiam map art Discord link.`,`?author≫Returns the bots author.`,`?ph <search term>≫Search PornHub.`,`?tpb <search term>≫Search The Pirate Bay.`,`?fact≫Returns a random fact.`,`?dick <player> <reason>≫Report player for being a dick.`,`?rules≫Lists the rules of the server.`,`?rule <1-47>≫Returns a specific rule of the internet.`,`?8ball <question>≫Ask the magic 8 ball something.`,`?ping <player>≫Show someone's ping.`,`?bestping≫Show who has the lowest ping.`,`?worstping≫Show who has the highest ping.`];
+const commands = [`?discord≫Get the Constantiam Discord link.`,`?mapart≫Get the Constantiam map art Discord link.`,`?author≫Returns the bots author.`,`?ph <search term>≫Search PornHub.`,`?tpb <search term>≫Search The Pirate Bay.`,`?fact≫Returns a random fact.`,`?dick <player> <reason>≫Report player for being a dick.`,`?rules≫Lists the rules of the server.`,`?rule <1-47>≫Returns a specific rule of the internet.`,`?8ball <question>≫Ask the magic 8 ball something.`,`?ping <player>≫Show someone's ping.`,`?bestping≫Show who has the lowest ping.`,`?worstping≫Show who has the highest ping.`,`?namemc <player>≫Look up username history for a player.`];
 var commandshtml = '';
 commands.forEach(function(element){
     commandshtml += `<tr>`;
